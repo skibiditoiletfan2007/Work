db9e074357e9bbcd07da70e1548776e397c698d6
skibiditoiletfan2007 <148602068+skibiditoiletfan2007@users.noreply.github.com>
2024-05-29 08:20:05 -0700

    Update sans.lua

--- sans.lua
+++ sans.lua
@@ -1,1 +1,11 @@
-SANNESSS
+local ReturnedTable = {}
+local 1DeathCounterQuote = {
+    "heya.",
+    "you look frustrated about something.",
+    "what? was it something i did?",
+    "no worries, my code is awesome.",
+}
+ReturnedTable.GetSansDCQuote = function(tablename)
+    return tablename
+end
+    
